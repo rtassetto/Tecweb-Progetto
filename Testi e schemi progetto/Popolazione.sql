209f6ed1e16934a895422cab768043d93243ebae
Matteo Lotto <superveemon@gmail.com>
2017-12-30 00:19:47 +0100

    Aggiunto  popolazione database

---
+++ Testi e schemi progetto/Popolazione.sql
@@ -0,0 +1,15 @@
+
+
+INSERT INTO `account` (`username`, `password`, `email`, `admin`, `datacreazione`) VALUES
+('admin', 'admin', 'admin@gm.it', 1, '2017-08-15 21:24:48'),
+('superveemon', 'pass', 'example@test.com', 1, '2017-12-13 03:26:19'),
+('user', 'user', 'user@buyer.com', 0, '2018-01-10 06:37:25');
+
+
+INSERT INTO `bundles` (`nome`, `descrizione`, `data`) VALUES
+('Primo pc', 'Il primo bundle offerto', '2017-12-30 04:21:02');
+
+INSERT INTO `prodotto` (`id`, `nome`, `categoria`, `descrizione`, `Valutazione`, `prezzo`) VALUES
+(1, 'Toshiba HDTB310EK3AA Canvio Basic ', 'HDD', 'Un tipo di hard disk', 0, 58),
+(2, 'LG 29UM59A Monitor 29\" 21:9 UltraWide LED', 'Monitor', 'Un monitor da gaming', 0, 79.99);
+
