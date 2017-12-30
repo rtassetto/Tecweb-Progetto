f8cf4a118ce696eb068e78a6dc3ff058aff4cb96
Matteo Lotto <superveemon@gmail.com>
2017-12-30 21:54:46 +0100

    Bugfix Database.sql

--- Testi e schemi progetto/Database.sql
+++ Testi e schemi progetto/Database.sql
@@ -1,10 +1,13 @@
+CREATE DATABASE Website;
+USE Website;
+
 CREATE TABLE Account(
     username varchar(20) PRIMARY KEY,
     password varchar(20),
     email varchar(50) UNIQUE,
     admin boolean,
     datacreazione datetime
-)
+);
 
 CREATE TABLE Prodotto(
   	id int PRIMARY KEY,
@@ -13,7 +16,7 @@ CREATE TABLE Prodotto(
     descrizione varchar(2000),
     Valutazione int,
     prezzo float
-)
+);
 
 CREATE TABLE PurchaseHistory(
 	idordine int PRIMARY KEY,
@@ -21,14 +24,14 @@ CREATE TABLE PurchaseHistory(
 	prodotto int REFERENCES id(Prodotto),
 	data datetime,
 	quantita int
-)
+);
 
 CREATE TABLE Carrello(
 	username varchar(20) REFERENCES username(Account),
 	prodotto int REFERENCES id(Prodotto),
 	quantita int,
     PRIMARY KEY (username,prodotto)
-)
+);
 
 CREATE TABLE Recensione(
 	username varchar(20) REFERENCES username(Account),
@@ -36,18 +39,17 @@ CREATE TABLE Recensione(
 	review varchar(1000),
 	voto int,
 	data datetime
-)
+);
 
 CREATE TABLE Bundles(
 	nome varchar(25),
 	descrizione varchar(300),
 	data datetime,
     PRIMARY KEY(nome, data)
-)
+);
 
 CREATE TABLE Bundleparts(
 	bundle varchar(25) REFERENCES nome(Bundles),
 	pezzo int REFERENCES id(Prodotto),
     PRIMARY KEY(bundle, pezzo)
-)
-;
+);
