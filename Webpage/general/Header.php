1b927a012200a8435a4b6536c9f0e6e68886c606
Unknown <zago-7@live.it>
2018-04-12 17:17:02 +0200

    Si ricomincia :(

--- Webpage/general/Header.php
+++ Webpage/general/Header.php
@@ -1,26 +1,26 @@
-    <header id="menu">
+    <div id="menu">
         <img id="logo" src="images/logo.png" alt="Logo sito"/>        
         <nav>
             <ul>
-                <li><a href="home.php">Home</a></li>
-                <li><a href="prodotti.php">Prodotti</a></li>
+                <li class="voci"><a href="home.php">Home</a></li>
+                <li class="voci"><a href="prodotti.php">Prodotti</a></li>
 				<?php
 				if(isset($_SESSION['login_user'])){
                 $isA=$_SESSION['admin'];   
                 if(!$isA){
-					echo '<li><a href="account.php">Account</a></li>';
-				    echo '<li><a href="carrello.php">Carrello</a></li>';
+					echo '<li class="voci"><a href="account.php">Account</a></li>';
+				    echo '<li class="voci"><a href="carrello.php">Carrello</a></li>';
 
                 }
                 else{
-                    echo '<li><a href="account.php">Account</a></li>';
-                    echo '<li><a href="adminmenu.php">Gestione sito</a></li>'; 
+                    echo '<li class="voci"><a href="account.php">Account</a></li>';
+                    echo '<li class="voci"><a href="adminmenu.php">Gestione sito</a></li>'; 
                 }
-                echo '<li><a href="php-script/logout.php">Logout</a></li>';
+                echo '<li class="voci"><a href="php-script/logout.php">Logout</a></li>';
 				}
 				else{
-				echo '<li><a href="login.php">Login</a></li>';
-				echo '<li><a href="register.php">Registrati</a></li>';
+				echo '<li class="voci"><a href="login.php">Login</a></li>';
+				echo '<li class="voci"><a href="register.php">Registrati</a></li>';
 				
 				}
 				?>
@@ -32,4 +32,4 @@
                     <input id="search" type="text" name="testo" placeholder="Cosa stai cercando?">
             </form> 
             </div>
-    </header>
+    </div>
