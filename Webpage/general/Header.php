b16d01b91b61137749b391199922830be4c958bc
Unknown <zago-7@live.it>
2018-04-17 18:51:23 +0200

    tolti link pagina stessa

--- Webpage/general/Header.php
+++ Webpage/general/Header.php
@@ -12,31 +12,42 @@
                 $isA=$_SESSION['admin'];   
                 if(!$isA){
 					echo '<li class="voci">';
-                    if($_SERVER["PHP_SELF"]=="/Tecweb-Progetto/Webpage/account.php")'<a>Account</a></li>';
+                    if($_SERVER["PHP_SELF"]=="/Tecweb-Progetto/Webpage/account.php"){echo '<a>Account</a></li>';}
                     else{echo '<a href="account.php">Account</a></li>'; }
-				    echo '<li class="voci"><a href="carrello.php">Carrello</a></li>';
+				    echo '<li class="voci">';
+                    if($_SERVER["PHP_SELF"]=="/Tecweb-Progetto/Webpage/carrello.php"){echo '<a>Carrello</a></li>';}
+                    else{echo '<a href="carrello.php">Carrello</a></li>';}
 
                 }
                 else{
-                    echo '<li class="voci"><a href="account.php">Account</a></li>';
-                    echo '<li class="voci"><a href="adminmenu.php">Gestione sito</a></li>'; 
+                    echo '<li class="voci">';
+                    if($_SERVER["PHP_SELF"]=="/Tecweb-Progetto/Webpage/account.php"){echo '<a>Account</a></li>';}
+                    else{echo '<a href="account.php">Account</a></li>'; }
+                    echo '<li class="voci">';
+                    if($_SERVER["PHP_SELF"]=="/Tecweb-Progetto/Webpage/adminmenu.php"){echo '<a>Gestione sito</a></li>';}
+                    else{echo '<a href="adminmenu.php">Gestione sito</a></li>';} 
                 }
                 echo '<li class="voci"><a href="php-script/logout.php">Logout</a></li>';
 				}
 				else{
-				echo '<li class="voci"><a href="login.php">Login</a></li>';
-				echo '<li class="voci"><a href="register.php">Registrati</a></li>';
+                echo '<li class="voci">';
+                if($_SERVER["PHP_SELF"]=="/Tecweb-Progetto/Webpage/login.php"){echo '<a>Login</a></li>';}
+                else{echo '<a href="login.php">Login</a></li>'; }
+                echo '<li class="voci">';
+                if($_SERVER["PHP_SELF"]=="/Tecweb-Progetto/Webpage/register.php"){echo '<a>Registrati</a></li>';}
+                else{echo '<a href="account.php">Registrati</a></li>'; }
+				
 				
 				}
 				?>
             <!--</ul>-->
         <!--</nav>-->
-            <div id="searchBar">
+            <li id="searchBar">
             <form action="prodotti.php" method="get">
                     <button id="searchButton" type="submit">Cerca</button>
                     <input id="search" type="text" name="testo" placeholder="Cosa stai cercando?">
             </form> 
-            </div>
+            </li>
             </ul>
         </nav>
     </div>
