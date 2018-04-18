e4dfb47edbe72a0402da0c62c1099e9e954f2a12
Unknown <zago-7@live.it>
2018-04-18 11:13:55 +0200

    immagine di validazione non più in px

--- Webpage/general/Footer.php
+++ Webpage/general/Footer.php
@@ -1,11 +1,10 @@
     <footer>
         <p>
             <a href="http://jigsaw.w3.org/css-validator/check/referer">
-            <img style="border:0;width:88px;height:31px"
-            src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Valido!" />
+            <img class="CSS" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Valido!" />
             </a>
         </p>
         
-        <p> Developed by Lotto Matteo, Tassetto Riccardo, Zago Davide</p>
+        <p class="corsivo"> Developed by Lotto Matteo, Tassetto Riccardo, Zago Davide</p>
 
     </footer>
