fa7234c470a0844844fa0aedbe6727447395e4ea
Davide <zago-7@live.it>
2018-07-02 16:12:58 +0200

    fix relazione

--- Webpage/general/Meta.php
+++ Webpage/general/Meta.php
@@ -3,9 +3,9 @@
     <meta name="keywords" content="ebook, ebook gratis, vendita ebook, e-book"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Lotto Matteo, Tassetto Riccardo, Zago Davide"/>
-    <link rel="stylesheet" type="text/css" href="style/style1.css"/>
+    <link rel="stylesheet" type="text/css" href="style/style.css"/>
     <link rel="stylesheet" href="style/print.css" media="print"/>
-	<link rel="stylesheet" type="text/css" media="only screen and (max-width:480px)" href="style/small1.css" />
+	<link rel="stylesheet" type="text/css" media="only screen and (max-width:480px)" href="style/small.css" />
     <link rel="stylesheet" type="text/css" media="print" href="css/print.css"/>
     <script type="text/javascript" src="javascript/script.js"></script>
     
